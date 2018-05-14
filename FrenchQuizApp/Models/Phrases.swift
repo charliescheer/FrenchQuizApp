a875cfd446d3359b7240f25740dc9cb69379a7dc
charliescheer <charlie.scheer@automattic.com>
2018-05-14 13:58:30 -0600

    Changed the comparing methods in Phrases using guard let instead of nested if let statements Added some error handling as a result of those guard lets

--- FrenchQuizApp/Models/Phrases.swift
+++ FrenchQuizApp/Models/Phrases.swift
@@ -8,40 +8,44 @@ extension Phrases {
     func compareUserAnswerToQuiz (quizState: Int, userAnswer: String) -> Double {
         var result: Double = 0.00
         
-            let ldDisatance = Double(self.LDPhraseCompare(userAnswer: userAnswer, quizState: quizState))
-            let answerLength = Double(self.returnQuizAnswer(quizState: quizState).count)
-            result = (answerLength - ldDisatance) / answerLength
-            print("percent result \(result)")
-            print("user answer \(userAnswer)")
-      
+        let ldDisatance = Double(self.doLDPhraseCompare(userAnswer: userAnswer, quizState: quizState))
+        
+        guard ldDisatance == 2.0 else {
+            return result
+        }
+        
+        let answerLength = Double(self.returnQuizAnswer(quizState: quizState).count)
+        result = (answerLength - ldDisatance) / answerLength
+        print("percent result \(result)")
+        print("user answer \(userAnswer)")
+  
         return result
     }
     
-    func LDPhraseCompare(userAnswer: String?, quizState: Int) -> Int {
+    func doLDPhraseCompare(userAnswer: String?, quizState: Int) -> Int {
         var result: Int = 0
         
+        guard let answer = userAnswer else {
+            result = 2
+            return result
+        }
+        
+        guard let quiz = self.frenchPhrase?.lowercased() else {
+            result = 2
+            return result
+        }
+        
+        guard answer == "NO ANSWER" else {
+            result = 2
+            return result
+        }
         
         if quizState == 0 {
-            if let answer = userAnswer {
-                if let quiz = self.frenchPhrase?.lowercased() {
-                    if answer != "NO ANSWER" {
-                        result = levenshtein(aStr: quiz, bStr: answer)
-                        print(result)
-                    }
-                }
-            }
-            
+                result = levenshtein(aStr: quiz, bStr: answer)
+                print(result)
         } else {
-            if let answer = userAnswer {
-                if let quiz = self.englishPhrase?.lowercased() {
-                        if answer != "NO ANSWER" {
-                        result = levenshtein(aStr: quiz, bStr: answer)
-                        print(result)
-                        } else {
-                            result = 2
-                    }
-                }
-            }
+            result = levenshtein(aStr: quiz, bStr: answer)
+            print(result)
         }
         
         return result
