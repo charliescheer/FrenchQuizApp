7b915daf5258a696568daafebf16e0bcdc9b9fe3
charliescheer <charlie.scheer@automattic.com>
2018-09-03 15:04:33 -0600

    Added the ability to create and insert new Noun objects from the addNounViewController

--- FrenchQuizApp/Nouns/Models/Nouns.swift
+++ FrenchQuizApp/Nouns/Models/Nouns.swift
@@ -2,5 +2,19 @@ import UIKit
 
 extension Nouns {
         //MARK: - Comparing Functions
-        
+    func setGenderMale() {
+        self.gender = constants.male
+    }
+    
+    func setGenderFemale() {
+        self.gender = constants.female
+    }
+}
+
+extension Nouns {
+    
+    enum constants {
+        static let male = "male"
+        static let female = "female"
+    }
 }
--- FrenchQuizApp/Nouns/Views/AddNounViewController.swift
+++ FrenchQuizApp/Nouns/Views/AddNounViewController.swift
@@ -6,9 +6,57 @@ class AddNounViewController:  UIViewController {
     @IBOutlet weak var englishTextField: UITextField!
     @IBOutlet weak var frenchTextField: UITextField!
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var genderSwitch: UISwitch!
+    
+    var dataResultsController = ManagedData.resultsController
+    var managedObjectContext = ManagedData.persistentContainer.viewContext
     
     
     @IBAction func addPhraseButtonWasPressed(_ sender: Any) {
+        if englishTextField.text == "" || frenchTextField.text == "" {
+            displayEmptyNounAlert()
+        } else {
+            let englishNoun = englishTextField.text!.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
+            let frenchNoun = frenchTextField.text!.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
+            
+            createNewNoun(english: englishNoun, french: frenchNoun)
+            clearUserFields()
+        }
     }
     
+    func createNewNoun(english: String, french: String) {
+        if let newNoun = NSEntityDescription.insertNewObject(forEntityName: "Nouns", into: managedObjectContext) as? Nouns {
+            newNoun.english = english
+            newNoun.french = french
+            newNoun.creationDate = NSDate() as Date
+            
+            if genderSwitch.isOn {
+                newNoun.setGenderFemale()
+            } else {
+                newNoun.setGenderMale()
+            }
+            
+            ManagedData.saveContext()
+        }
+        
+        do {
+            try dataResultsController.performFetch()
+        } catch {
+            print("fetch failed")
+        }
+        
+        tableView.reloadData()
+    }
+    
+    func clearUserFields() {
+        englishTextField.text! = ""
+        frenchTextField.text! = ""
+        genderSwitch.isOn = false
+    }
+    
+    func displayEmptyNounAlert () {
+        let alert = UIAlertController(title: "Enter Noun", message: "Please enter a Noun for both fields", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Okay", style: .default, handler: nil))
+        self.present(alert, animated: true)
+    }
 }
