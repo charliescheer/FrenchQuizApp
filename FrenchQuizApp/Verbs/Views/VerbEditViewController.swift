68578280b807471977daeafbbf6e0fa073bc4d67
charliescheer <charlie.scheer@automattic.com>
2018-09-24 15:36:07 -0600

    Added the edit and delete functions for editverbVC

--- FrenchQuizApp/Verbs/Views/VerbEditViewController.swift
+++ FrenchQuizApp/Verbs/Views/VerbEditViewController.swift
@@ -5,6 +5,7 @@ class VerbEditViewController: UIViewController {
     var verb : Verbs?
     var conjugationDictionary : [String : [String : String]] = [ : ]
     var tenseArray = ["Présent", "Imparfait", "Futur", "Passé", "Passé simple"]
+    var mode = "View"
     
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var englishTextLabel: UILabel!
@@ -13,6 +14,8 @@ class VerbEditViewController: UIViewController {
     @IBOutlet weak var timesIncorrectLabel: UILabel!
     @IBOutlet weak var correctInARowLabel: UILabel!
     @IBOutlet weak var learnedLabel: UILabel!
+    @IBOutlet weak var deleteSaveButton: UIBarButtonItem!
+    @IBOutlet weak var editViewButton: UIBarButtonItem!
     
     
     
@@ -20,6 +23,35 @@ class VerbEditViewController: UIViewController {
     @IBAction func backTapped(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
+    
+    @IBAction func editTapped(_ sender: Any) {
+        if mode == "View" {
+            enterEdit()
+        } else if mode == "Edit" {
+            updatePhrase()
+            enterView()
+        } else {
+            print("The mode is out of range")
+        }
+    }
+    
+    @IBAction func resetTapped(_ sender: Any) {
+        if let currentVerb = verb {
+            displayResetAlert(verb: currentVerb)
+        }
+    }
+    
+    @IBAction func deleteSaveTapped(_ sender: Any) {
+        if mode == "View" {
+            displayDeleteAlert()
+        } else if mode == "Edit" {
+            updatePhrase()
+            enterView()
+        } else {
+            print("Error: The mode is not correct")
+        }
+    }
+
     
     override func viewDidLoad() {
         guard let currentVerb = verb else {
@@ -29,6 +61,34 @@ class VerbEditViewController: UIViewController {
         conjugationDictionary = currentVerb.unarchiveDictionary()
         
         setupView()
+    }
+    
+    func enterView() {
+        mode = "View"
+        editViewButton.title = "Edit"
+        deleteSaveButton.title = "Delete"
+        
+        
+        englishTextLabel?.isUserInteractionEnabled = false
+        frenchTextLabel?.isUserInteractionEnabled = false
+    }
+    
+    func enterEdit() {
+        mode = "Edit"
+        editViewButton.title = "View"
+        deleteSaveButton?.title = "Save"
+        
+        
+        englishTextLabel?.isUserInteractionEnabled = true
+        frenchTextLabel?.isUserInteractionEnabled = true
+    }
+    
+    
+    func updatePhrase() {
+        if verb != nil {
+            verb?.english = englishTextLabel.text
+            verb?.french = frenchTextLabel.text
+        }
     }
     
     func setupView() {
@@ -46,6 +106,48 @@ class VerbEditViewController: UIViewController {
             }
         
         }
+    }
+    
+    func displayDeleteAlert () {
+        if let currentVerb = verb {
+            let alert = UIAlertController(title: "Are you sure?", message: "This will delete '\(currentVerb.english ?? "No Phrase Selected")'", preferredStyle: .alert)
+            
+            alert.addAction(UIAlertAction(title: "Okay", style: .default, handler: {action in self.didConfirmDeletePhrase()}))
+            alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+            
+            self.present(alert, animated: true)
+        }
+    }
+    
+    func displayResetAlert (verb: Verbs) {
+        let alert = UIAlertController(title: "Reset Counts", message: "This will set the counts for '\(verb.english ?? "No Phrase Selected")' back to 0, are you sure?", preferredStyle: .alert)
+        
+        alert.addAction(UIAlertAction(title: "Okay", style: .default, handler: {action in self.didConfirmResetCounts()}))
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        
+        
+        self.present(alert, animated: true)
+    }
+    
+    func didConfirmResetCounts (){
+        verb!.resetCountPhraseCounts()
+        setupView()
+    }
+    
+    func didConfirmDeletePhrase() {
+        let context = ManagedData.persistentContainer.viewContext
+        
+        if let currentVerb = verb {
+            context.delete(currentVerb)
+            returnsToList()
+        }
+    }
+    
+    func returnsToList () {
+        let storyboard = UIStoryboard(name: "Nouns", bundle: nil)
+        let controller = storyboard.instantiateViewController(withIdentifier: "addNoun")
+        self.dismiss(animated: true, completion: nil)
+        self.present(controller, animated: true, completion: nil)
     }
     
 }
