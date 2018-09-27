c047a1757c3fa3d78492338e31f9b230a7002b9f
charliescheer <charlie.scheer@automattic.com>
2018-09-26 22:01:49 -0600

    Added the ability to edit the conjugations for saved verbs Added the ability to save changes to core data

--- FrenchQuizApp/Verbs/Views/VerbConjugationViewController.swift
+++ FrenchQuizApp/Verbs/Views/VerbConjugationViewController.swift
@@ -7,14 +7,14 @@ class VerbConjugationViewController: UIViewController {
     var conjugationDictionary : [String : [String : String]] = [ : ]
     var tenseDictionary : [String : String] = [ : ]
     var articleArray = ["je", "tu", "il", "nous", "vous", "ils"]
+    var mode = "View"
+    var hasChanged = false
     
     @IBOutlet weak var tableView: UITableView!
-    
     
     @IBAction func backTapped(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
-    
     
     override func viewDidLoad() {
         guard let currentVerb = verb, let currentTense = tense else {
@@ -27,6 +27,60 @@ class VerbConjugationViewController: UIViewController {
             print(tenseDictionary)
         }
     }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        guard let currentTense = tense else {
+            return
+        }
+        
+        if verb != nil {
+            if hasChanged == true {
+                conjugationDictionary[currentTense] = tenseDictionary
+                let mutatedDictionary = mutateDictionaryToData()
+                
+                verb!.conjugationDictionary = mutatedDictionary
+                ManagedData.saveContext()
+            }
+        }
+    }
+    
+    func displayEditAlert(sender: ArticleCell) {
+        let alert = UIAlertController(title: "Enter Conjugation", message: "Enter a new conjugation for \(sender.conjugationTextLabel.text ?? "current conjugation")", preferredStyle: .alert)
+        
+        
+        
+        alert.addTextField { (textField) in
+            textField.placeholder = sender.conjugationTextLabel.text
+        }
+        
+        
+        
+        let submitAction = UIAlertAction(title: "Save", style: .default) { (action) in
+            let text = alert.textFields![0].text
+            alert.textFields![0].keyboardAppearance = UIKeyboardAppearance.alert
+            
+            if text != "" {
+                self.hasChanged = true
+                sender.conjugationTextLabel.text = text
+                self.tenseDictionary[sender.articleTextLabel.text!] = text
+                print(self.tenseDictionary)
+            }
+        }
+        
+        alert.addAction(submitAction)
+        alert.addAction(UIAlertAction(title: "Cancel", style: .default, handler: nil))
+    
+        
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    
+    func mutateDictionaryToData() -> Data {
+        let data: Data = NSKeyedArchiver.archivedData(withRootObject: conjugationDictionary)
+        
+        return data
+    }
+
 }
 
 extension VerbConjugationViewController: UITableViewDelegate, UITableViewDataSource {
@@ -41,6 +95,10 @@ extension VerbConjugationViewController: UITableViewDelegate, UITableViewDataSou
         articleCell.conjugationTextLabel.text = tenseDictionary[articleArray[indexPath.row]]
         
         return articleCell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        displayEditAlert(sender: tableView.cellForRow(at: indexPath) as! ArticleCell)
     }
 }
 
